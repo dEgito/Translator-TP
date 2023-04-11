383ac95b10e940e9c6120899775f0e18cc45ca0d
Danielly Egito <75611712+dEgito@users.noreply.github.com>
2023-04-11 10:03:16 -0300

    init: configuração inicial do repositório

---
+++ tp-translator/src/App.js
@@ -0,0 +1,11 @@
+// import CopyToClipboard from 'react-copy-to-clipboard';
+
+function App() {
+  return (
+    <div className="App">
+      <p>TP-TRANSLATOR</p>
+    </div>
+  );
+}
+
+export default App;
